02f43111141d267b54d4624a035e43a77061fc57
nkunievsky <nadavkunievsky@gmail.com>
2024-04-11 15:42:28 -0500

    design

--- assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-function toggleAbstract(abstractId) {
-    var element = document.getElementById(abstractId);
-    if (element.style.display === 'none') {
-        element.style.display = 'block';
-    } else {
-        element.style.display = 'none';
-    }
-}
